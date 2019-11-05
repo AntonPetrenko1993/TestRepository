2f27a0c6eecb162a97393c13a89f0e9a27eb2a7b
Anton Petrenko <anton.petrenko@ua.akvelon.com>
2019-11-05 17:43:48 +0200

    Add project files.

---
+++ TestProjectForTeamCity.Tests/CalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace TestProjectForTeamCity.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Sum_2_plus_2_returned_4()
+        {
+
+        }
+
+    }
+}
---
+++ TestProjectForTeamCity/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Text;
+
+namespace TestProjectForTeamCity
+{
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
